2c2b3481827c3bc88c1e41f06a9da1ce0e14bd06
cityrank <mr.empty@mail.ru>
2024-11-09 20:21:35 +0100

    Update tokenLoader.js

--- map/tokenLoader.js
+++ map/tokenLoader.js
@@ -2,7 +2,6 @@
 const encodedCloudKitToken = 'NTk4ZDUyODc2OGZlYzM1YWUxMDQxN2QzMzEzZmQ0YWU2ZmM2YzY1OTA3YTJlMmU3YmY4ODQ5MWYwZWZmOWQwYQ==';
 const encodedMapboxToken = 'cGsuZXlKMUlqb2liV2xyYVhScmJ6RWlMQ0poSWpvaVkyMHdlbUp4YURWek1EVmhlREpxY3pnME5uVjNNRzFqYnlKOS44TU5TMDdjc2dJSmtVWFRHalppYVlB'; // New encoded value
 
-// Decoding function
 function decodeToken(encodedToken) {
     try {
         return atob(encodedToken);
@@ -13,35 +12,35 @@ function decodeToken(encodedToken) {
 }
 
 // Initialize CloudKit and Mapbox with decoded tokens
-function initializeServices() {
-    try {
-        const cloudKitToken = decodeToken(encodedCloudKitToken);
-        const mapboxToken = decodeToken(encodedMapboxToken);
+export function initializeServices() {
+    return new Promise((resolve, reject) => {
+        try {
+            const cloudKitToken = decodeToken(encodedCloudKitToken);
+            const mapboxToken = decodeToken(encodedMapboxToken);
 
-        // Initialize CloudKit
-        CloudKit.configure({
-            containers: [{
-                containerIdentifier: 'iCloud.com.mikita.mapapp',
-                apiTokenAuth: { apiToken: cloudKitToken, persist: true },
-                environment: 'production'
-            }]
-        });
+            // Initialize CloudKit
+            CloudKit.configure({
+                containers: [{
+                    containerIdentifier: 'iCloud.com.mikita.mapapp',
+                    apiTokenAuth: { apiToken: cloudKitToken, persist: true },
+                    environment: 'production'
+                }]
+            });
 
-        // Initialize Mapbox
-        mapboxgl.accessToken = mapboxToken;
-        const map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [0, 0],
-            zoom: 2,
-            projection: 'globe'
-        });
+            // Initialize Mapbox
+            mapboxgl.accessToken = mapboxToken;
+            const map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [0, 0],
+                zoom: 2,
+                projection: 'globe'
+            });
 
-
-    } catch (error) {
-        console.error("Error initializing services:", error);
-    }
+            map.on('load', () => resolve(map));
+        } catch (error) {
+            console.error("Error initializing services:", error);
+            reject(error);
+        }
+    });
 }
-
-// Call initializeServices once the file is loaded
-initializeServices();
